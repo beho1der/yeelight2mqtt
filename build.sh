ca4d0f2900a14fbf5df3eb875449a2c6cd456d7d
captain <capain.perreiro@gmail.com>
2026-03-03 16:50:30 +0700

    opkg

---
+++ build.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+# Параметры пакета
+PKG_NAME="yeelight2mqtt"
+PKG_VERSION=$(sed -n '/Version = /s/.*"\([^"]*\)".*/\1/p' run.go)
+PKG_MAINTAINER="captain <captain.perreiro@gmail.com>"
+PKG_DESC="service add yeelight device to mqtt"
+PKG_SECTION="misc"
+PKG_PRIORITY="standard"
+BUILD_DIR="build"
+PKG_DIR="${BUILD_DIR}/${PKG_NAME}"
+
+# Очистка и создание структуры
+rm -rf ${BUILD_DIR}
+mkdir -p ${PKG_DIR}/control
+mkdir -p ${PKG_DIR}/data
+mkdir -p ${PKG_DIR}/data/etc
+mkdir -p ${PKG_DIR}/data/etc/${PKG_NAME}
+mkdir -p ${PKG_DIR}/data/etc/init.d
+mkdir -p ${PKG_DIR}/data/usr/bin
+
+# Создание исполняемого файла с tinygo и upx архитектура arm_cortex-a7
+PKG_ARCH="arm_cortex-a7_neon-vfpv4"
+env GOOS=linux GOARCH=arm GOARM=7 tinygo build -no-debug -o ${PKG_DIR}/data/usr/bin/${PKG_NAME}
+upx --best --lzma ${PKG_DIR}/data/usr/bin/${PKG_NAME}
+chmod +x ${PKG_DIR}/data/usr/bin/${PKG_NAME}
+
+# debian-binary
+cat <<EOF > ${BUILD_DIR}/debian-binary
+2.0
+EOF
+
+# control
+cat <<EOF > ${PKG_DIR}/control/control
+Package: ${PKG_NAME}
+Version: ${PKG_VERSION}
+Architecture: ${PKG_ARCH}
+Maintainer: ${PKG_MAINTAINER}
+Description: ${PKG_DESC}
+Section: ${PKG_SECTION}
+Priority: ${PKG_PRIORITY}
+EOF
+
+cat <<EOF > ${PKG_DIR}/control/postinst
+#!/bin/sh
+
+/etc/init.d/${PKG_NAME} enable
+/etc/init.d/${PKG_NAME} start
+
+exit 0
+EOF
+
+cat <<EOF > ${PKG_DIR}/control/prerm
+#!/bin/sh
+
+/etc/init.d/${PKG_NAME} stop
+/etc/init.d/${PKG_NAME} disable
+
+exit 0
+EOF
+
+cat <<EOF > ${PKG_DIR}/control/conffiles
+/etc/${PKG_NAME}/config.yaml
+EOF
+
+# data
+cat <<EOF > ${PKG_DIR}/data/etc/${PKG_NAME}/config.yaml
+lights:
+- host: 192.168.50.2
+  name: light-1-example
+  oldfirmware: false
+- host: 192.168.50.3
+  name: light-2-example
+  oldfirmware: false
+lightpollingrate:
+  seconds: 10
+mqttsettings:
+  host: localhost
+  port: 1883
+  tls: false
+  user: ""
+  password: ""
+  basetopic: y2m
+  qos: 2
+debug: false
+EOF
+
+cat <<EOF > ${PKG_DIR}/data/etc/init.d/${PKG_NAME}
+!/bin/sh /etc/rc.common
+
+# Запуск после сетевых служб
+START=88
+USE_PROCD=1
+
+NAME=${PKG_NAME}
+PROG=/usr/bin/${PKG_NAME}
+
+start_service() {
+    procd_open_instance
+    # Указываем путь к бинарнику
+    procd_set_param command "\$PROG"
+    # Respawn: порог 3600с, таймаут 5с, максимум 5 попыток
+    procd_set_param respawn \${respawn_threshold:-3600} \${respawn_timeout:-5} \${respawn_retry:-5}
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_close_instance
+}
+
+reload_service() {
+    # Перезагружаем именно инстанс сервиса
+    procd_send_signal "\$PROG"
+}
+EOF
+
+# 4. Сборка пакета
+tar -czvf ${BUILD_DIR}/control.tar.gz  ${PKG_DIR}/control
+tar -czvf ${BUILD_DIR}/data.tar.gz  ${PKG_DIR}/data
+
+ar rv  ${BUILD_DIR}/${PKG_NAME}_${PKG_VERSION}_${PKG_ARCH}.ipk ${BUILD_DIR}/debian-binary ${BUILD_DIR}/control.tar.gz ${BUILD_DIR}/data.tar.gz
+
+echo "Пакет ${PKG_NAME}_${PKG_VERSION}_${PKG_ARCH}.ipk создан в папке ${BUILD_DIR}"
